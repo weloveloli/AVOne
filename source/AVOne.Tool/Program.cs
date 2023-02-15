1bf7c0e3cfce8f5d6226b2c612aeb26aa73f0a9a
esfak47 <esfak47@qq.com>
2023-02-15 16:33:41 +0800

    update code

---
+++ source/AVOne.Tool/Program.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2023 Weloveloli. All rights reserved.
+// Licensed under the GPLv2 License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureHostConfiguration(configHost =>
+    {
+        configHost.SetBasePath(Directory.GetCurrentDirectory());
+        configHost.AddEnvironmentVariables(prefix: "AVONE_");
+        configHost.AddCommandLine(args);
+    })
+    .Build();
+
+// Application code should start here.
+
+await host.RunAsync();
