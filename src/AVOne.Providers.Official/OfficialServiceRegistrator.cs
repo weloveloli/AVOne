537c79043da7d2f27e62bf825ee60a5526321d28
esfak47 <esfak47@qq.com>
2023-04-18 10:05:05 +0800

    fix docker

--- src/AVOne.Providers.Official/OfficialServiceRegistrator.cs
+++ src/AVOne.Providers.Official/OfficialServiceRegistrator.cs
@@ -17,41 +17,41 @@ namespace AVOne.Providers.Official
 
         public void RegisterServices(IServiceCollection serviceCollection)
         {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
-            serviceCollection.AddHttpClient(Constants.Official, (client) =>
-            {
-                client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50");
-                client.DefaultRequestHeaders.AcceptLanguage.Clear();
-                client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("zh-CN"));
-                client.DefaultRequestVersion = HttpVersion.Version20;
-            });
+            // ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            // serviceCollection.AddHttpClient(Constants.Official, (client) =>
+            // {
+            //     client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50");
+            //     client.DefaultRequestHeaders.AcceptLanguage.Clear();
+            //     client.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue("zh-CN"));
+            //     client.DefaultRequestVersion = HttpVersion.Version20;
+            // });
 
-            serviceCollection.AddHttpClient(AVOneConstants.Download, (client) =>
-            {
-                client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50");
-            }).ConfigurePrimaryHttpMessageHandler((builder) =>
-            {
-                var handler = new HttpClientHandler
-                {
-                    ServerCertificateCustomValidationCallback = (m, c, ch, e) => { return true; },
-                    AutomaticDecompression = DecompressionMethods.All
-                };
-                return handler;
+            // serviceCollection.AddHttpClient(AVOneConstants.Download, (client) =>
+            // {
+            //     client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50");
+            // }).ConfigurePrimaryHttpMessageHandler((builder) =>
+            // {
+            //     var handler = new HttpClientHandler
+            //     {
+            //         ServerCertificateCustomValidationCallback = (m, c, ch, e) => { return true; },
+            //         AutomaticDecompression = DecompressionMethods.All
+            //     };
+            //     return handler;
 
-            })
-                .ConfigureHttpMessageHandlerBuilder(sp => new SocketsHttpHandler
-                {
-                    // Connect Timeout.
-                    ConnectTimeout = TimeSpan.FromSeconds(30),
-                    // TCP Keep Alive.
-                    KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always,
-                    KeepAlivePingDelay = TimeSpan.FromSeconds(30),
-                    KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
+            // })
+            //     .ConfigureHttpMessageHandlerBuilder(sp => new SocketsHttpHandler
+            //     {
+            //         // Connect Timeout.
+            //         ConnectTimeout = TimeSpan.FromSeconds(30),
+            //         // TCP Keep Alive.
+            //         KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always,
+            //         KeepAlivePingDelay = TimeSpan.FromSeconds(30),
+            //         KeepAlivePingTimeout = TimeSpan.FromSeconds(30),
 
-                    // Connection Pooling.
-                    PooledConnectionLifetime = TimeSpan.FromMinutes(10),
-                    PooledConnectionIdleTimeout = TimeSpan.FromSeconds(90),
-                });
+            //         // Connection Pooling.
+            //         PooledConnectionLifetime = TimeSpan.FromMinutes(10),
+            //         PooledConnectionIdleTimeout = TimeSpan.FromSeconds(90),
+            //     });
 
         }
     }
