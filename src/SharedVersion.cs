1ac44a20bcfa8903f4d698030c3c19a5dd633622
esfak47 <esfak47@qq.com>
2023-05-27 22:14:04 +0800

    修复了一些问题

--- src/SharedVersion.cs
+++ src/SharedVersion.cs
@@ -3,5 +3,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.6")]
+[assembly: AssemblyFileVersion("0.2.6")]
