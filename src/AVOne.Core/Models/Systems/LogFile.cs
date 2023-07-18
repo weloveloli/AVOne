88e447615ba393721c2fb2b3ee85a88eea8e10e9
esfak47 <esfak47@qq.com>
2023-07-18 15:07:44 +0800

    Add Logs

---
+++ src/AVOne.Core/Models/Systems/LogFile.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2023 Weloveloli. All rights reserved.
+// See License in the project root for license information.
+
+#nullable disable
+#pragma warning disable CS1591
+
+namespace AVOne.Models.Systems
+{
+    public class LogFile
+    {
+        /// <summary>
+        /// Gets or sets the date created.
+        /// </summary>
+        /// <value>The date created.</value>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date modified.
+        /// </summary>
+        /// <value>The date modified.</value>
+        public DateTime DateModified { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size.
+        /// </summary>
+        /// <value>The size.</value>
+        public long Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+    }
+}
