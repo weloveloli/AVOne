be99a00c10b32aa59a7cf0d53f44bd7863c69a1a
esfak47 <esfak47@qq.com>
2023-08-16 15:04:00 +0800

    Update code

---
+++ src/AVOne.Providers.Official/Plugin.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2023 Weloveloli. All rights reserved.
+// See License in the project root for license information.
+
+namespace AVOne.Providers.Official
+{
+    using AVOne.Common.Plugins;
+    using AVOne.Configuration;
+    using AVOne.IO;
+
+    public class Plugin : BasePlugin<OfficialPluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+        }
+
+        public override string Name => "Official";
+
+        public override Guid Id => new Guid("45e3fde0-e857-4cfe-9696-fe874ec40a2f");
+
+        public override string Description => "Official plugin for AVOne";
+    }
+}
